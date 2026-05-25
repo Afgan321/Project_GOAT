e62208feaec201a99daa8c92bffcd2e1562416e8
Afgan321 <fahrulsiagian@gmail.com>
2026-05-25 20:28:51 +0700

    revisi hangman_indo.py

--- hangman_indo.py
+++ hangman_indo.py
@@ -1,11 +1,29 @@
-#with open ("datagw.txt", "r")  as f :
-
-
 import random
 
-with open("datagw.txt", "r", encoding="utf-8") as file:
+with open("datagw.txt", "r") as file:
     baris = file.readlines()
-print(baris)
-kata = random.choice(baris).strip()
 
-print(kata)
+kata = random.choice(baris).strip()
+kataTersembunyi = kata[0] + ((len(kata)-1) * "*")
+a = 1
+b = len(kata) - 1
+
+while True:
+    print(kataTersembunyi)
+    tebakanKata = str(input("masukkan tebakanlu: ", ).lower())
+
+    if tebakanKata == kata:
+        print("benar")
+        
+    else:
+        print("tebakan lu salah")
+        a += 1
+        b -= 1
+        kataTersembunyi = kata[:a] + kataTersembunyi[a:]
+
+    if b == 0:
+        print("\nplenger")
+        print(f"kata yang benar itu: {kata}")
+        break
+
+    print()
